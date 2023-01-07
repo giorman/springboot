3c31a88df5038259678d2401dbea8cdbedf8d09e
Giorman Rodriguez <giorman.rodriguez@pragma.com.co>
2023-01-06 19:50:42 -0500

    first commit

---
+++ src/main/java/com/digitalthinking/HelloResource.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.digitalthinking;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author sotobotero
+ */
+@RestController
+public class HelloResource {
+    
+    @GetMapping("hello")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Spring boot";
+    }
+    
+}
